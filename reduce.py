64ba1a4ab0dc5414d029bd85c3194aff6f304aae
Holden Higgins <hhiggins@stuy.edu>
2018-05-01 13:06:17 -0400

    efficiencty

--- reduce.py
+++ reduce.py
@@ -18,17 +18,12 @@ words = open_f()
 
 #find frequency of single word
 def word_freq(word, f):
-    lst = [1 for w in f if w == word] #creates list of 1's for when word is found
-    if lst == []:
-        return 0
-    return reduce((lambda x, y: x + y), lst) #adds up frequency of word using reduce
-
+   return reduce((lambda x,y: x+(1 if y==word else 0)),f,0)
 #print word_freq('hello', sample)
 
 #find frequency of group of words
 def words_freq(phrase, f):
-    split_phrase = phrase.split()
-    #print(split_phrase)
+
     #finds if phrase is found by iterating through list of words
     lst = [1 for w in range(len(f)) if split_phrase == sample[w: w+len(split_phrase)]]
     if lst == []: #no appearance of phrase
@@ -41,8 +36,22 @@ def words_freq(phrase, f):
 def most_freq(f):
     #creates a set of words without repeating
     unique_words = {x for x in f}
+    w={}
+    for x in unique_words:
+        w[x]=0
+    for x in f:
+        w[x]+=1
+    longest=""
+    longfreq=0
+    for x in unique_words:
+        if w[x]>longfreq:
+            longfreq=w[x]
+            longest=x
+    return longest+" "+str(longfreq)
     #turns back to list so indexOf can be used
+    """"print 2
     lst = [x for x in unique_words]
+    print 3
     counts = [word_freq(x, f) for x in lst]
     return lst[counts.index(max(counts))] #max num corresponds with word
 
@@ -54,6 +63,7 @@ def most_freq(f):
     #     if count > current_highest:
     #         current_word = w
     #         current_highest = count
-    # return current_word
-
-#print most_freq(sample)
+    # return current_word"""
+#print word_freq("the",words)
+print most_freq(words)
+#print "hi"
